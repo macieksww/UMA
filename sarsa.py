306cc156395a154b51f547fa6920400de044e0b2
ZubikLena <80119227+ZubikLena@users.noreply.github.com>
2022-01-02 18:14:03 +0100

    Inna wersja algorytmu  sarsa

--- sarsa.py
+++ sarsa.py
@@ -1,162 +1,86 @@
 import gym
-import sys
-import pyglet
 import numpy as np
-import matplotlib.pyplot as plt 
-import time
+import math
+import matplotlib.pyplot as plt
 
-def get_discrete_state(state):
-    discrete_state = (state - env.observation_space.low)/discrete_os_win_size
-    return tuple(discrete_state.astype(np.int))
-
-def exploit_or_explore(discrete_state):
-    if np.random.random() > epsilon:
-        action = np.argmax(q_table[discrete_state])
-    else:
-        action = np.random.randint(0, env.action_space.n)
-    return action
-
-def adjust_exploration_coefficient(epsilon, episode):
-    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
-        epsilon -= epsilon_decay_value
-    return epsilon
-    
-# For stats
+# containers for statistics data
 ep_rewards = []
 aggr_ep_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
-STATS_EVERY = 100
-episode_counter = 0
 
-DISCRETE_OS_SIZE = [20, 20, 20, 20]
-LEARNING_RATE = 0.1
-DISCOUNT = 0.9
-EPISODES = 4000
-SHOW_EVERY = 1000
-epsilon = 0.1
-START_EPSILON_DECAYING = 1
-END_EPSILON_DECAYING = EPISODES//2
-epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
+class CartPoleAgent():
+    def __init__(self, buckets=(1, 1, 6, 12), num_episodes=10000, min_lr=0.1, min_epsilon=0.1, discount=0.98, decay=25):
+        self.buckets = buckets
+        self.num_episodes = num_episodes
+        self.min_lr = min_lr
+        self.min_epsilon = min_epsilon
+        self.discount = discount
+        self.decay = decay
 
+        self.env = gym.make('CartPole-v0')
 
-env = gym.make("CartPole-v0")
+        # [position, velocity, angle, angular velocity]
+        self.upper_bounds = [self.env.observation_space.high[0], 0.5, self.env.observation_space.high[2], math.radians(50) / 1.]
+        self.lower_bounds = [self.env.observation_space.low[0], -0.5, self.env.observation_space.low[2], -math.radians(50) / 1.]
 
-discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
-q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
-discrete_state = get_discrete_state(env.reset())
+        self.q_table = np.zeros(self.buckets + (self.env.action_space.n,))
 
-print(env.action_space.n)
-print(env.reset())
-done = False
+    def discretize_state(self, obs):
+        discretized = list()
+        for i in range(len(obs)):
+            scaling = (obs[i] + abs(self.lower_bounds[i])) / (self.upper_bounds[i] - self.lower_bounds[i])
+            new_obs = int(round((self.buckets[i] - 1) * scaling))
+            new_obs = min(self.buckets[i] - 1, max(0, new_obs))
+            discretized.append(new_obs)
+        return tuple(discretized)
 
+    def choose_action(self, state):
+        if (np.random.random() < self.epsilon):
+            return self.env.action_space.sample() 
+        else:
+            return np.argmax(self.q_table[state])
 
-    # Starting State:
-    # All observations are assigned a uniform random value in [-0.05..0.05]
-    
-    # Episode Termination:
-    #     Pole Angle is more than 12 degrees (from stability).
-    #     Cart Position is more than 2.4 (center of the cart reaches the edge of
-    #     the display).
-    #     Episode length is greater than 200.
-    
-    # Solved Requirements:
-    #     Considered solved when the average return is greater than or equal to
-    #     195.0 over 100 consecutive trials.
+    def update_sarsa(self, state, action, reward, new_state, new_action):
+        self.q_table[state][action] += self.learning_rate * (reward + self.discount * (self.q_table[new_state][new_action]) - self.q_table[state][action])
 
-for episode in range(EPISODES):
-    
-    episode_counter += 1
-    start_time = time.time()
-    ep_reward = 0 
-    discrete_state = get_discrete_state(env.reset())
-    done = False
+    def get_epsilon(self, t):
+        return max(self.min_epsilon, min(1., 1. - math.log10((t + 1) / self.decay)))
 
-    if episode % SHOW_EVERY == 0:
-        render = True
-        print(episode)
-    else:
-        render = False
+    def get_learning_rate(self, t):
+        return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
 
-    while not done:
-        
-        # Actions:
-        # 0: Push cart to the left
-        # 1: Push cart to the right
-        
-        action = exploit_or_explore(discrete_state)
-        # print("Action: " + str(action))
-        
-        # Observation:
-        # Cart Position: [-2.4, 2,4]
-        # Cart Velocity: [-Inf, Inf]
-        # Pole Angle: [-0.209 rad (-12 deg), 0.209 rad (12 deg)] from stability
-        # Pole angular velocity: [-Inf, Inf]
-        
-        # Reward:
-        # 1: for every taken action, including terminal state
-        
-        new_state, reward, done, _ = env.step(action)
-        new_discrete_state = get_discrete_state(new_state)
-        new_action = exploit_or_explore(new_discrete_state)
-        ep_reward += reward
-        # print("Reward: ", reward)
-        # print("New state: ", new_state)
-        # print("New discrete state: ", new_state)
-        
-        if episode % SHOW_EVERY == 0:
-            env.render()
-        
-        if not done:
+    def train(self):
+        for e in range(self.num_episodes):
+            current_state = self.discretize_state(self.env.reset())
+
+            self.learning_rate = self.get_learning_rate(e)
+            self.epsilon = self.get_epsilon(e)
+            done = False
+
+            reward_sum = 0
+            while not done:
+                action = self.choose_action(current_state)
+                obs, reward, done, _ = self.env.step(action)
+                reward_sum += reward
+                new_state = self.discretize_state(obs)
+                new_action = self.choose_action(new_state)
+                self.update_sarsa(current_state, action, reward, new_state, new_action)
+                current_state = new_state
+
+            aggr_ep_rewards['ep'].append(e)
+            ep_rewards.append(reward_sum)
+            aggr_ep_rewards['avg'].append(np.mean(ep_rewards))
+            aggr_ep_rewards['min'].append(min(ep_rewards))
+            aggr_ep_rewards['max'].append(max(ep_rewards))
             
-            print(action, new_action)
-
-            # Maximum possible Q value in next step (for new state)
-            max_future_q = q_table[new_discrete_state + (new_action,)]
-
-            # Current Q value (for current state and performed action)
-            current_q = q_table[discrete_state + (action,)]
-
-            # And here's our equation for a new Q value for current state and action
-            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
-            # self.sarsa_table[state][action] += self.learning_rate * (reward + self.discount * (self.sarsa_table[new_state][new_action]) - self.sarsa_table[state][action])
+        print('Finished training!')            
 
 
-            # Update Q table with new Q value
-            q_table[discrete_state + (action,)] = new_q
-            # def update_sarsa(self, state, action, reward, new_state, new_action):
-            # self.sarsa_table[state][action] += self.learning_rate * (reward + self.discount * (self.sarsa_table[new_state][new_action]) - self.sarsa_table[state][action])
-
-
-        # Simulation ended (for any reson) - if goal position is achived - update Q value with reward directly
-        elif new_state[2] >= - 0.209 and new_state[2] <= 0.209:
-            q_table[discrete_state + (action,)] = 0
-
-        discrete_state = new_discrete_state
-        
-    # Update of epsilon (exploration coefficient)
-        
-    epsilon = adjust_exploration_coefficient(epsilon, episode)
-    ep_rewards.append(ep_reward)
+if __name__ == "__main__":
+    agent = CartPoleAgent()
+    agent.train()
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="avg rewards")
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max rewards")
+    plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min rewards")
+    plt.legend(loc=4)
+    plt.show()
     
-    if not episode % STATS_EVERY:
-        average_reward = sum(ep_rewards[-STATS_EVERY:])/STATS_EVERY
-        aggr_ep_rewards['ep'].append(episode)
-        aggr_ep_rewards['avg'].append(average_reward)
-        aggr_ep_rewards['max'].append(max(ep_rewards[-STATS_EVERY:]))
-        aggr_ep_rewards['min'].append(min(ep_rewards[-STATS_EVERY:]))
-        print(f'Episode: {episode:>5d}, average reward: {average_reward:>4.1f}, current epsilon: {epsilon:>1.2f}')
-    
-    if episode % 10 == 0:
-        np.save(f"qtables/{episode}-qtable.npy", q_table)
-        
-    end_time = time.time()
-    print("Episode" + str(episode_counter) + "time: " + str(end_time - start_time))
-    
-
-plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="average rewards")
-plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max rewards")
-plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min rewards")
-plt.legend(loc=4)
-plt.show()
-
-env.close()
-
